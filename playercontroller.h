9b6dbe453875151322ea0413e6e4f711ef006353
Don Silvio <svobodacom1@gmail.com>
2025-01-31 16:47:29 +0200

    Added PlaylistPanel for songs, inherited PleyerController class from QAbstractListModel and changing some functions, added virtual functions from base class, created new functions addAudio, removeAudio

--- playercontroller.h
+++ playercontroller.h
@@ -1,45 +1,63 @@
 #ifndef PLAYERCONTROLLER_H
 #define PLAYERCONTROLLER_H
 
+#include <QAbstractListModel>
 #include <QMediaPlayer>
 #include <QObject>
 
-class PlayerController : public QObject
+class AudioInfo;
+
+class PlayerController : public QAbstractListModel
 {
     Q_OBJECT
-
-    Q_PROPERTY(int currentSongIndex READ currentSongIndex NOTIFY currentSongIndexChanged)
-    Q_PROPERTY(int songCount READ songCount NOTIFY songCountChanged)
     Q_PROPERTY(bool playing READ playing NOTIFY playingChanged)
+    Q_PROPERTY(AudioInfo* currentSong READ currentSong WRITE setCurrentSong NOTIFY currentSongChanged)
 
 
 public:
+    enum Role
+    {
+        AudioTitleRole = Qt::UserRole + 1,
+        AudioAuthorNameRole,
+        AudioSourceRole,
+        AudioImageSourceRole,
+        AudioVideoSourceRole
+    };
+
     explicit PlayerController(QObject *parent = nullptr);
 
-    int currentSongIndex() const;
-    int songCount() const;
     bool playing() const;
 
     // first way to expose C++ functions to QML using Q_INVOKABLE macro
     Q_INVOKABLE void switchToNextSong();
+    Q_INVOKABLE void switchToPreviousSong();
+
+    // QAbstractItemModel interface
+    virtual int rowCount(const QModelIndex &parent) const override;
+    virtual QVariant data(const QModelIndex &index, int role) const override;
+    virtual QHash<int, QByteArray> roleNames() const override;
+
+    AudioInfo* currentSong() const;
+    void setCurrentSong(AudioInfo* newCurrentSong);
 
 public slots:
     // second way to expose C++ functions to QML put them to public slots modificator
-    void switchToPreviousSong();
-    void playPause();
     void changeAudioSource(const QUrl &source);
+    void playPause();
+    void addAudio(const QString& title, const QString& authorName, const QUrl& audioSource,
+                  const QUrl& imageSource, const QUrl& videoSource = QUrl());
+
+    void removeAudio(int index);
 
 signals:
-
-    void currentSongIndexChanged();
-    void songCountChanged();
     void playingChanged();
+    void currentSongChanged();
 
 private:
-    int m_currentSongIndex = 0;
-    int m_songCount = 3;
     bool m_playing = false;
     QMediaPlayer m_mediaPlayer;
+    QList<AudioInfo*> m_audioList;
+    AudioInfo* m_currentSong = nullptr;
 };
 
 #endif // PLAYERCONTROLLER_H
